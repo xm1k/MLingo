1e24cf203c4feb9a826dc2b26e05019082818ca1
hsematrix <xmik2022@gmail.com>
2024-11-20 03:13:50 +0300

    no-comments

--- app/src/main/java/com/example/mlingo/MainActivity.kt
+++ app/src/main/java/com/example/mlingo/MainActivity.kt
@@ -41,8 +41,6 @@ import androidx.core.view.WindowInsetsControllerCompat
 import androidx.compose.foundation.border
 import com.example.mlingo.ui.theme.MLingoTheme
 
-
-
 val Nothing = FontFamily(
     Font(R.font.nothing)
 )
@@ -51,10 +49,6 @@ val Anon = FontFamily(
     Font(R.font.anon)
 )
 
-
-
-
-
 data class Question(
     val question: String,
     val options: List<String>,
@@ -62,15 +56,12 @@ data class Question(
     val explanation: String
 )
 
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Устанавливаем цвет статус-бара такой же, как у приложения
-        val appBackgroundColor = Color(0xFF121212).toArgb() // Преобразуем Compose цвет в ARGB
-        window.statusBarColor = appBackgroundColor // Устанавливаем цвет
+        val appBackgroundColor = Color(0xFF121212).toArgb()
+        window.statusBarColor = appBackgroundColor
 
         setContent {
             MLingoTheme {
@@ -79,8 +70,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
 
 @Composable
 fun QuizScreen() {
@@ -103,54 +92,50 @@ fun QuizScreen() {
     var selectedAnswer by remember { mutableStateOf<Int?>(null) }
     var isAnswerCorrect by remember { mutableStateOf<Boolean?>(null) }
     var explanation by remember { mutableStateOf("") }
-    var isAnswerSelected by remember { mutableStateOf(false) } // Флаг для блокировки кнопок
+    var isAnswerSelected by remember { mutableStateOf(false) }
 
     val currentQuestion = questions[currentQuestionIndex]
 
-    // Функция для обработки выбранного ответа
     fun checkAnswer(answerIndex: Int) {
-        if (!isAnswerSelected) { // Если ответ еще не выбран
+        if (!isAnswerSelected) {
             selectedAnswer = answerIndex
             isAnswerCorrect = answerIndex == currentQuestion.correctAnswerIndex
             explanation = if (isAnswerCorrect == true) "" else currentQuestion.explanation
-            isAnswerSelected = true // Устанавливаем флаг, чтобы блокировать кнопки
+            isAnswerSelected = true
         }
     }
 
-    // Функция для перехода к следующему вопросу
     fun nextQuestion() {
         currentQuestionIndex = (currentQuestionIndex + 1) % questions.size
         selectedAnswer = null
         isAnswerCorrect = null
         explanation = ""
-        isAnswerSelected = false // Сбрасываем флаг для следующего вопроса
+        isAnswerSelected = false
     }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF121212)) // Темный фон для всего приложения
+            .background(Color(0xFF121212))
     ) {
-        // Размещаем вопрос в верхней части
         Column(
             modifier = Modifier
-                .align(Alignment.Center) // Выравниваем по центру
-                .fillMaxWidth() // Занимаем всю ширину
-                .padding(bottom = 128.dp, start = 16.dp, end = 16.dp), // Указываем padding по бокам
-            horizontalAlignment = Alignment.CenterHorizontally // Центрируем содержимое по горизонтали
+                .align(Alignment.Center)
+                .fillMaxWidth()
+                .padding(bottom = 128.dp, start = 16.dp, end = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TypingText(fullText = currentQuestion.question) // Печатаем текст вопроса посимвольно
+            TypingText(fullText = currentQuestion.question)
         }
 
-        // Панель с результатом внизу
         if (selectedAnswer != null) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.BottomCenter) // Панель с результатом всегда снизу
-                    .fillMaxHeight(0.2f) // Растягиваем панель на 20% от экрана
-                    .padding(0.dp) // Убираем внутренние отступы
-                    .background(Color(0xFF212121)) // Темный фон панели
+                    .align(Alignment.BottomCenter)
+                    .fillMaxHeight(0.2f)
+                    .padding(0.dp)
+                    .background(Color(0xFF212121))
             ) {
                 ResultPanel(
                     isAnswerCorrect = isAnswerCorrect,
@@ -160,53 +145,50 @@ fun QuizScreen() {
             }
         }
 
-        // Размещение кнопок в центре
         val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-        val panelHeight = screenHeight * 0.2f // Высота панели, 20% от высоты экрана
+        val panelHeight = screenHeight * 0.2f
 
         Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(bottom = panelHeight + 32.dp, start = 16.dp, end = 16.dp) // Используем вычисленную высоту панели
+                .padding(bottom = panelHeight + 32.dp, start = 16.dp, end = 16.dp)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Ваши кнопки
             currentQuestion.options.chunked(2).forEach { rowOptions ->
                 Row(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp) // Горизонтальные отступы между кнопками
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
                     rowOptions.forEach { option ->
                         Button(
                             onClick = {
-                                if (!isAnswerSelected) { // Если ответ еще не выбран
+                                if (!isAnswerSelected) {
                                     checkAnswer(currentQuestion.options.indexOf(option))
                                 }
                             },
                             modifier = Modifier
-                                .height(100.dp) // Сохраняем высоту кнопки
-                                .weight(1f) // Кнопки равной ширины
-                                .padding(bottom = 16.dp) // Одинаковые отступы сверху, снизу, слева и справа
+                                .height(100.dp)
+                                .weight(1f)
+                                .padding(bottom = 16.dp)
                                 .border(2.dp, when {
-                                    selectedAnswer == currentQuestion.options.indexOf(option) && isAnswerCorrect == true -> Color.Green // Правильный ответ
-                                    selectedAnswer == currentQuestion.options.indexOf(option) && isAnswerCorrect == false -> Color.Red  // Неправильный ответ
-                                    else -> Color.White // Белая рамка по умолчанию
-                                }), // Цвет рамки в зависимости от ответа
+                                    selectedAnswer == currentQuestion.options.indexOf(option) && isAnswerCorrect == true -> Color.Green
+                                    selectedAnswer == currentQuestion.options.indexOf(option) && isAnswerCorrect == false -> Color.Red
+                                    else -> Color.White
+                                }),
                             colors = androidx.compose.material3.ButtonDefaults.buttonColors(
-                                containerColor = Color(0xFF121212) // Темный фон кнопки
+                                containerColor = Color(0xFF121212)
                             ),
-                            shape = androidx.compose.foundation.shape.RoundedCornerShape(0.dp) // Убираем скругления
+                            shape = androidx.compose.foundation.shape.RoundedCornerShape(0.dp)
                         ) {
                             Text(
                                 text = option,
                                 fontSize = 16.sp,
                                 textAlign = TextAlign.Center,
-                                color = Color.White, // Белый текст
-                                style = TextStyle(fontFamily = Anon) // Шрифт Anon
+                                color = Color.White,
+                                style = TextStyle(fontFamily = Anon)
                             )
                         }
-
                     }
                 }
             }
@@ -214,21 +196,19 @@ fun QuizScreen() {
     }
 }
 
-
 @Composable
 fun TypingText(
     fullText: String,
-    typingSpeed: Long = 35L // Скорость печати в миллисекундах
+    typingSpeed: Long = 35L
 ) {
-    var displayedText by remember { mutableStateOf("") } // Отображаемый текст
-    var currentIndex by remember { mutableStateOf(0) } // Текущий индекс символа
+    var displayedText by remember { mutableStateOf("") }
+    var currentIndex by remember { mutableStateOf(0) }
 
-    // Анимация добавления символов
     LaunchedEffect(fullText) {
         for (index in fullText.indices) {
             currentIndex = index
-            displayedText = fullText.substring(0, currentIndex + 1) // Обновляем отображаемый текст
-            kotlinx.coroutines.delay(typingSpeed) // Ждем перед добавлением следующего символа
+            displayedText = fullText.substring(0, currentIndex + 1)
+            kotlinx.coroutines.delay(typingSpeed)
         }
     }
 
@@ -242,7 +222,6 @@ fun TypingText(
     )
 }
 
-
 @Composable
 fun ResultPanel(
     isAnswerCorrect: Boolean?,
@@ -250,24 +229,20 @@ fun ResultPanel(
     onSwipeNext: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var offsetX by remember { mutableStateOf(0f) } // Смещение панели
-    var dragStartPosition by remember { mutableStateOf(0f) } // Начальная точка свайпа
-    var isPanelVisible by remember { mutableStateOf(false) } // Видимость панели
-    var alpha by remember { mutableStateOf(1f) } // Прозрачность
+    var offsetX by remember { mutableStateOf(0f) }
+    var dragStartPosition by remember { mutableStateOf(0f) }
+    var isPanelVisible by remember { mutableStateOf(false) }
+    var alpha by remember { mutableStateOf(1f) }
 
-    // Показываем панель с анимацией
     LaunchedEffect(Unit) {
         isPanelVisible = true
     }
 
-    // Получаем ширину экрана в dp
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
 
-    // Расчет прозрачности в зависимости от смещения
     alpha = remember(offsetX) {
-        // Переводим смещение в отношение от ширины экрана и ограничиваем от 0 до 1
-        val maxSwipeDistance = screenWidth.value // ширина экрана в dp
-        (-offsetX / maxSwipeDistance).coerceIn(-1f, 1f) // Прозрачность увеличивается по мере свайпа влево
+        val maxSwipeDistance = screenWidth.value
+        (-offsetX / maxSwipeDistance).coerceIn(-1f, 1f)
     }
 
     AnimatedVisibility(
@@ -277,27 +252,23 @@ fun ResultPanel(
     ) {
         Box(
             modifier = modifier
-                .background(Color(0xFF212121)) // Темный фон панели
+                .background(Color(0xFF212121))
                 .padding(16.dp)
-                .offset { IntOffset(offsetX.toInt(), 0) } // Смещение по X
+                .offset { IntOffset(offsetX.toInt(), 0) }
                 .pointerInput(Unit) {
                     detectHorizontalDragGestures(
                         onDragStart = { startPoint ->
-                            dragStartPosition = startPoint.x // Сохраняем начальную точку
+                            dragStartPosition = startPoint.x
                         },
                         onDragEnd = {
-                            // Проверяем смещение после окончания жеста
                             if (offsetX < -screenWidth.value * 0.25f) {
-                                // Если свайп больше порога, переходим к следующему вопросу
-                                offsetX = 0f // Сбрасываем смещение панели
+                                offsetX = 0f
                                 onSwipeNext()
                             } else {
-                                // Если свайп меньше порога, возвращаем панель назад
                                 offsetX = 0f
                             }
                         },
                         onHorizontalDrag = { _, dragAmount ->
-                            // Обновляем смещение панели
                             offsetX += dragAmount
                         }
                     )
@@ -307,39 +278,31 @@ fun ResultPanel(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(), // Возвращаем предыдущее расположение
-                verticalArrangement = Arrangement.Center // Центрируем содержимое
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
             ) {
-                // Текст результата с изменяющейся прозрачностью
                 if (isAnswerCorrect != null) {
                     Text(
                         text = if (isAnswerCorrect) "Правильно!" else "Неправильно!",
                         color = if (isAnswerCorrect) Color.Green.copy(alpha = (1f - alpha).coerceIn(0f, 1f)) else Color.Red.copy(alpha = (1f - alpha).coerceIn(0f, 1f)),
-                        fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
-                        style = TextStyle(fontFamily = Nothing),
-                        textAlign = TextAlign.Center
+                        fontSize = 24.sp,
+                        style = TextStyle(fontFamily = Anon)
                     )
-                }
-
-                // Пояснение с изменяющейся прозрачностью
-                if (isAnswerCorrect == false && explanation.isNotEmpty()) {
-                    Text(
-                        text = explanation,
-                        color = Color.White.copy(alpha = (1f - alpha).coerceIn(0f, 1f)), // Прозрачность меняется по мере свайпа
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Normal,
-                        modifier = Modifier.padding(top = 8.dp),
-                        style = TextStyle(fontFamily = Anon),
-                        textAlign = TextAlign.Center
-                    )
+                    if (!isAnswerCorrect) {
+                        Text(
+                            text = explanation,
+                            color = Color.White,
+                            fontSize = 18.sp,
+                            textAlign = TextAlign.Center,
+                            style = TextStyle(fontFamily = Anon)
+                        )
+                    }
                 }
             }
         }
     }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
